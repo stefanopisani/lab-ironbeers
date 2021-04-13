ccf5dd97f2d1ea52d240c449c703d87e88183642
Stefano Pisani <stefano.pisani92@gmail.com>
2021-04-13 17:57:43 +0100

    ironBeers

--- app.js
+++ app.js
@@ -14,12 +14,27 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+app.get('/beers', async (req, res) => {
+  let beersFromApi = await punkAPI.getBeers();
+  res.render('beers', {
+    beersFromApi
+  });
+});
+
+app.get('/random-beer', async (req, res) => {
+  let randomBeerFromApi = await punkAPI.getRandom();
+  res.render('random-beer', {
+    randomBeerFromApi
+  });
+});
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
